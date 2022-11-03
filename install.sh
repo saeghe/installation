d0f5fd6d4fa1987194a3d142c62d39d09f40b523
Morteza <morteza@protonmail.com>
2022-11-03 14:33:38 +0100

    Check PHP vesion. Add information to add credentials. Remove auto source

--- install.sh
+++ install.sh
@@ -2,6 +2,27 @@
 
 set -e
 
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+YELLOW='\033[1;33m'
+DEFAULT_COLOR='\033[0m' # No Color
+
+if ! command -v php &> /dev/null
+then
+    echo "PHP if not installed"
+    exit
+fi
+PHP_VERSION=$(php --version | grep "PHP "\[0-9] -o)
+
+if [ "${PHP_VERSION}" == "PHP 8" ]
+then
+  PHP_VERSION=$(php --version | grep "PHP "\[0-9])
+  echo "${PHP_VERSION} detected"
+else
+  echo -e "${RED}Required PHP version not detected! Saeghe needs PHP >= 8.0 ${DEFAULT_COLOR}"
+  exit
+fi
+
 USER=${USER:-$(id -u -n)}
 HOME="${HOME:-$(getent passwd $USER 2>/dev/null | cut -d: -f6)}"
 HOME="${HOME:-$(eval echo ~$USER)}"
@@ -14,18 +35,18 @@ echo "Make Saeghe directory"
 mkdir -p $SAEGHE_HOME
 
 SAEGHE_LATEST_RELEASE=$(curl -s -H "Accept: application/vnd.github+json" https://api.github.com/repos/saeghe/saeghe/releases | grep "tag_name" | grep v\[0-9a-z\.]\\+ -o | head -n 1)
-echo Saeghe version: $SAEGHE_LATEST_RELEASE
+echo -e "Saeghe version: ${GREEN}${SAEGHE_LATEST_RELEASE}${DEFAULT_COLOR}"
 
 CLI_LATEST_RELEASE=$(curl -s -H "Accept: application/vnd.github+json" https://api.github.com/repos/saeghe/cli/releases | grep "tag_name" | grep v\[0-9a-z\.]\\+ -o | head -n 1)
-echo CLI version: $CLI_LATEST_RELEASE
+echo -e "CLI version: ${GREEN}${CLI_LATEST_RELEASE}${DEFAULT_COLOR}"
 
-echo Start downloading version $SAEGHE_LATEST_RELEASE
+echo -e "Start downloading Saeghe version: ${GREEN}${SAEGHE_LATEST_RELEASE}${DEFAULT_COLOR}"
 curl -s -L https://github.com/saeghe/saeghe/zipball/$SAEGHE_LATEST_RELEASE > $SAEGHE_HOME/saeghe.zip
 
-echo Start downloading version $CLI_LATEST_RELEASE
+echo -e "Start downloading CLI version: ${GREEN}${CLI_LATEST_RELEASE}${DEFAULT_COLOR}"
 curl -s -L https://github.com/saeghe/cli/zipball/$CLI_LATEST_RELEASE > $SAEGHE_HOME/cli.zip
 
-echo "Download finished"
+echo -e "${GREEN}Download finished${DEFAULT_COLOR}"
 
 echo "Install Saeghe"
 rm -fR $SAEGHE_SOURCE_PATH
@@ -51,13 +72,11 @@ if [ "$DEFAULT_SHELL" == "/bin/zsh" ]
 then
   echo "Add Saeghe to zsh"
   echo $EXPORT_PATH >> $HOME/.zshrc
-  zsh
-  source $HOME/.zshrc
 else
   echo "Add Saeghe to bash"
   echo $EXPORT_PATH >> $HOME/.bashrc
-  bash
-  source $HOME/.bahrc
 fi
 
-echo Installation finished successfully. Enjoy.
+echo -e "${YELLOW}- Please open a new terminal to start working with Saeghe.${DEFAULT_COLOR}"
+echo -e "${YELLOW}- Please add your credential using the credential command. Visit https://saeghe.com/documentations/credential-command ${DEFAULT_COLOR}"
+echo -e "${GREEN}Installation finished successfully. Enjoy.${DEFAULT_COLOR}"
